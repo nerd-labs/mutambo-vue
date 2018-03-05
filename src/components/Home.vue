cc677feecb0febf8ebf6a1a96e5b89ed98ca40c2
Robbert Wolfs <robbertwolfs@gmail.com>
2018-03-05 20:17:49 +0100

    feat(build): add pug to build
    now you can use lang="pug" inside your templates
    
    #29

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,21 @@
-<template>
-  <a class="button button--white" href="#">
-    create tournament
-  </a>
+<template lang="pug">
+  a.button.button--white(href="#")
+    span create
+    | tournament
 </template>
 
 <script>
-export default {
-  name: 'home',
-}
+export default {};
 </script>
+
+<style lang="scss">
+.button {
+  background:teal;
+  width: 75%;
+  margin: 0 auto;
+
+  > span {
+    color: hotpink;
+  }
+}
+</style>
