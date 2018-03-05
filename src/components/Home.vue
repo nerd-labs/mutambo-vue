950cb1a118b76ad2c235800fa52c0f7c5496899a
Robbert Wolfs <robbertwolfs@gmail.com>
2018-03-05 22:09:02 +0100

    fix(pug): PR comment fixes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,11 @@
 <template lang="pug">
-  a.button.button--white(href="#")
-    span create
-    | tournament
+  div
+    a.button.button--white(href="#") create tournament
 </template>
-
-<script>
-export default {};
-</script>
 
 <style lang="scss">
 .button {
   background:teal;
-  width: 75%;
   margin: 0 auto;
-
-  > span {
-    color: hotpink;
-  }
 }
 </style>
