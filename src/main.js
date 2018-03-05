eebce2d6682e394a2e43024af5896f092c1272e0
Wart Claes <wart.claes@gmail.com>
2018-03-05 21:29:25 +0100

    fix(logo): Fix PR comments

--- src/main.js
+++ src/main.js
@@ -1,25 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-// import Vuetify from 'vuetify';
 
-// import 'prismjs/prism';
-// import 'prismjs/themes/prism.css';
-// import 'prismjs/themes/prism-solarizedlight.css';
-// import 'vuetify/dist/vuetify.min.css';
 import App from './App.vue';
 import Home from './components/Home.vue';
 import Logo from './components/Logo.vue';
-// import Config from './components/Config.vue';
 
 Vue.component('mut-logo', Logo);
 
 Vue.use(VueRouter);
-// Vue.use(Vuetify);
 
 const router = new VueRouter({
   routes: [
     { path: '/', component: Home },
-    // { path: '/config', component:/ Config }
   ]
 });
 
