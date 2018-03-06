067f2e0d40f54375551953acf522c6e4de343dc3
Robbert Wolfs <robbertwolfs@gmail.com>
2018-03-06 09:15:25 +0100

    refactor(input): remove input component bc of vuetify

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,3 @@
 <template>
   <router-link to="create" class="button button--white"> create tournament</router-link>
 </template>
-
-<script>
-export default {
-  name: "home"
-};
-</script>
--- src/main.js
+++ src/main.js
@@ -7,12 +7,9 @@ import 'vuetify/dist/vuetify.min.css';
 import App from './App.vue';
 import Home from './components/Home.vue';
 import Create from './components/Create.vue';
-import Input from './components/Input.vue';
 
 Vue.use(VueRouter);
 Vue.use(Vuetify);
-
-Vue.component('mut-input', Input);
 
 const router = new VueRouter({
   routes: [
