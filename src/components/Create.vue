5459770215dc42fe851d7336c1dd71a3f74e8383
Robbert Wolfs <robbertwolfs@gmail.com>
2018-03-06 09:13:12 +0100

    feat(create): add vuetify components and setup of create page
    #10

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,12 +1,61 @@
 <template>
   <div>
-    <mut-input label="Name"></mut-input>
-    <mut-input label="Number of players" type="number"></mut-input>
+   <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs6 offset-xs3>
+         <v-text-field
+          label="Name"
+          v-model="name"
+          required
+        ></v-text-field>
+      </v-flex>
+      <v-flex xs6 offset-xs3>
+         <v-text-field
+          label="Number of players"
+          v-model="totalPlayers"
+          type="number"
+          required
+        ></v-text-field>
+      </v-flex>
+      <v-flex xs6 offset-xs3>
+           <v-btn-toggle v-model="type">
+              <v-btn flat value="knockout">
+                Knockout
+              </v-btn>
+              <v-btn flat value="league">
+                League
+              </v-btn>
+              <v-btn flat value="groupstage-knockout">
+                Groupstage & Knockout
+              </v-btn>
+            </v-btn-toggle>
+      </v-flex>
+      <v-flex xs6 offset-xs3>
+           <v-btn color="primary" @click="submit()">Submit</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
   </div>
 </template>
 
  <script>
-export default {};
+export default {
+  data: () => ({
+    name: "",
+    totalPlayers: undefined,
+    type: "knockout"
+  }),
+  methods: {
+    submit() {
+      alert("see console");
+      console.table({
+        name: this.name,
+        totalPlayers: this.totalPlayers,
+        type: this.type
+      });
+    }
+  }
+};
 </script>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
 import App from './App.vue';
 import Home from './components/Home.vue';
 import Create from './components/Create.vue';
 import Input from './components/Input.vue';
 
 Vue.use(VueRouter);
+Vue.use(Vuetify);
 
 Vue.component('mut-input', Input);
 
