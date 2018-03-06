1363c05bf037ddf8215b2c96c3cc1fbad8913f11
Robbert Wolfs <robbertwolfs@gmail.com>
2018-03-06 21:41:12 +0100

    fix(components): add vuetify components to Home and Create page
    improve create page with validation etc...

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,63 +1,43 @@
-<template>
-  <div>
-   <v-container grid-list-md text-xs-center>
-    <v-layout row wrap>
-      <v-flex xs6 offset-xs3>
-         <v-text-field
-          label="Name"
-          v-model="name"
-          required
-        ></v-text-field>
-      </v-flex>
-      <v-flex xs6 offset-xs3>
-         <v-text-field
-          label="Number of players"
-          v-model="totalPlayers"
-          type="number"
-          required
-        ></v-text-field>
-      </v-flex>
-      <v-flex xs6 offset-xs3>
-           <v-btn-toggle v-model="type">
-              <v-btn flat value="knockout">
-                Knockout
-              </v-btn>
-              <v-btn flat value="league">
-                League
-              </v-btn>
-              <v-btn flat value="groupstage-knockout">
-                Groupstage & Knockout
-              </v-btn>
-            </v-btn-toggle>
-      </v-flex>
-      <v-flex xs6 offset-xs3>
-           <v-btn color="primary" @click="submit()">Submit</v-btn>
-      </v-flex>
-    </v-layout>
-  </v-container>
-  </div>
+<template lang="pug">
+  div
+    v-container(grid-list-md)
+      v-form(v-model="valid" lazy-validation ref="form")
+        v-layout(row wrap)
+          v-flex(xs6 offset-xs3)
+            v-text-field(label="Name" v-model="name" required :rules="nameRules")
+
+          v-flex(xs6 offset-xs3)
+            v-text-field(label="Number of players" v-model="totalPlayers" required :rules="totalPlayerRules" type="number")
+
+          v-flex(xs6 offset-xs3)
+              v-btn-toggle(v-model="type")
+                  v-btn(color="primary white--text" flat value="knockout") Knockout
+                  v-btn(color="primary white--text" flat value="league") League
+                  v-btn(color="primary white--text" flat value="groupstage-knockout") Groupstate & Knockout
+
+          v-flex(xs6 offset-xs3)
+              v-btn(color="primary" @click="submit" :disabled="!valid") Submit
 </template>
 
  <script>
 export default {
   data: () => ({
+    valid: true,
     name: "",
-    totalPlayers: undefined,
+    nameRules: [v => !!v || "Name is required"],
+    totalPlayers: 2,
+    totalPlayerRules: [
+      v => !!v || "Number of players is required",
+      v => v >= 2 || "There should be at least 2 players"
+    ],
     type: "knockout"
   }),
   methods: {
-    submit() {
-      alert("see console");
-      console.table({
-        name: this.name,
-        totalPlayers: this.totalPlayers,
-        type: this.type
-      });
+    submit () {
+      if (!this.$refs.form.validate()) {
+        return;
+      }
     }
   }
 };
 </script>
-
-<style lang="scss">
-
-</style>
