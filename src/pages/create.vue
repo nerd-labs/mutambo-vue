f1bbd04534aa59952078de85a7b4f983fe91b8d6
Robbert Wolfs <robbertwolfs@gmail.com>
2018-03-08 09:54:01 +0100

    feat(team): create team detail page

--- src/pages/create.vue
+++ src/pages/create.vue
@@ -44,7 +44,7 @@ export default {
         numberOfPlayers: this.totalPlayers
       });
 
-      this.$router.push('/', );
+      this.$router.push('/teams');
     }
   }
 };
---
+++ src/pages/teams.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+    div
+      v-container(fluid grid-list-md)
+        v-layout(row wrap)
+          v-flex(d-flex xs12 sm6 md4 lg3 xl2 v-for="n in numberOfPlayers")
+            mut-create-team
+</template>
+
+<script>
+export default {
+  computed: {
+    numberOfPlayers() {
+      return parseInt(this.$store.state.tournaments.find(
+        tournament => tournament.name === "Euricom"
+      ).numberOfPlayers);
+    }
+  }
+};
+</script>
