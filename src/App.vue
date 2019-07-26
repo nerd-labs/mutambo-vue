554309ee3e599995132626e0a2f30749edf79aa3
Freek Mencke <info@toxsickproductions.com>
2019-07-26 17:38:14 +0200

    correct cli gen

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,29 @@
-<template lang="pug">
-  #app
-    .statusbar.u-background--gradient
-    router-view
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
 #app {
-  height: 100%;
-  width: 100%;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-
-.statusbar {
-  -webkit-app-region: drag;
-  display: block;
-  height: 22px;
-  position: fixed;
-  width: 100%;
-  z-index: 10;
-  &::before {
-    background-color: rgba(255, 255, 255, 0.75);
-    bottom: 0;
-    content: "";
-    left: 0;
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-
-  & + .page {
-    height: calc(100% - 22px) !important;
-    position: relative;
-    top: 22px; /* same height as statusbar */
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
 }
 </style>
