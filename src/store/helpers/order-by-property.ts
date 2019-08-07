08c792e2ac01fb3b65324738c46301a2920ea382
Wart Claes <wart.claes@gmail.com>
2019-08-07 15:35:56 +0200

    fix(helpers): fix order by property

--- src/store/helpers/order-by-property.ts
+++ src/store/helpers/order-by-property.ts
@@ -1,8 +1,9 @@
-export function orderByProperty(...prop: any): any {
-  const args = Array.prototype.slice.call(arguments, 1);
+export function orderByProperty(...argss: string[]): any {
+  const prop = argss[0];
+  const args: any = Array.prototype.slice.call(argss, 1);
   return (a: any, b: any) => {
     const equality = a[prop] - b[prop];
-    if (equality === 0 && arguments.length > 1) {
+    if (equality === 0 && argss.length > 1) {
       return orderByProperty.apply(null, args)(a, b);
     }
     return equality;
